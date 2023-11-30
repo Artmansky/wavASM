afcac49e46c67d1fd9858e0f82b3ce92634126b2
Artmansky <tomaszartmanski@gmail.com>
2023-11-30 22:24:06 +0100

    Projekt start na prawde!

---
+++ wavMONO/wavMONO/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wavMONO
+{
+    public partial class Form1 : Form
+    {
+        [DllImport(@"C:\Users\Tomek\Documents\Asembler\wavMONO\x64\Debug\wavASM.dll")]
+        static extern int MyProc1(int a, int b);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = MyProc1(3,8).ToString();
+        }
+    }
+}
